d62a090ef083d234c745a9cda3f9a2b3c12a8974
sarah <s.lindner@mein.gmx>
2021-01-29 19:26:16 +0100

    toggle navbar

--- src/main/resources/static/js/application.js
+++ src/main/resources/static/js/application.js
@@ -1,3 +1,23 @@
 const tagline = document.getElementById('tagline');
 tagline.innerHTML = 'Added with JavaScript';
 
+function toggleNavLinks (id) {
+    var links = document.getElementById(id);
+    if (links != null) {
+        if (links.style.display === "block") {
+            links.style.display = 'none';
+        } else {
+            links.style.display = 'block';
+        }
+        return false;
+    }
+}
+
+// export default {
+//     data() {
+//         return {
+//             isOpen: false;
+//         }
+//     },
+// }
+
